6b1188b9aa278ee59920b796e60ebfeb45daffaa
ojan <88722063+zshnrg@users.noreply.github.com>
2022-11-06 08:07:09 +0700

    Update deleteGame.c

--- src/deleteGame.c
+++ src/deleteGame.c
@@ -6,12 +6,12 @@ void deleteGame(TabKata *listGame, Queue queueGame) {
 
     printf("Berikut ini adalah daftar game yang tersedia\n");
     TulisIsi(*listGame);
-    printf("Masukkan nomor game yang akan dihapus: ");
+    printf("\nMasukkan nomor game yang akan dihapus: ");
     GetCommand();
 
     for (int i = 0; i < currentWord.Length; i++) {
         comm = comm * 10;
-        comm += currentWord.TabWord[i] = '0';
+        comm += currentWord.TabWord[i] - '0';
     }
 
     if (comm > 5 && !IsMemberQ(queueGame, (*listGame).TK[comm - 1])) {
